941dd4ee19f5ecda6d4774c6214fb080813c4b9d
Deyan Sirakov <deyan.sirakov2006@abv.bg>
2023-04-10 12:23:43 +0300

    Done with the java learning

--- java/Java-Stuff/Java-Stuff/src/HelloWorld.java
+++ java/Java-Stuff/Java-Stuff/src/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        // Just kidding, I already mastered Java. :D
     }
 }
